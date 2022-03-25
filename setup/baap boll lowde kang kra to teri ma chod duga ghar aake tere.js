992be092c0cc8ba4c812df48c8552a10b2a0167a
GOD INVISIBLE <101255902+invisibleofficial11@users.noreply.github.com>
2022-03-25 08:52:05 -0700

    Create baap boll lowde kang kra to teri ma chod duga ghar aake tere.js

---
+++ setup/baap boll lowde kang kra to teri ma chod duga ghar aake tere.js
@@ -0,0 +1,18 @@
+GAND MAR DUGA = await : in ("python_lga_ke_chod_duga")
+          print("madrchod")lowde
+await :
+ in (hack)))
+                  java dall duga gand me tere lowde ke baal)
+                  {
+                  }
+ alive_emoji "hfghskkfythgjudtrgfhdsfgtiujhkgfbvnxdesyt=76hjgftgjndig"
+java_mc))
+[buttonurl://git.
+ }
+              
+               await int(str(LEGEND_INVISIBLE_OP)".edit.send.978576645)"}{
+                 in its wait : edit =m 
+                  print{java}"""
+                         op-l
+                                      java output ma chod duga sabki 
+                                      
